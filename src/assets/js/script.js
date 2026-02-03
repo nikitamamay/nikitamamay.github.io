a7ddf18608a0873ab84cf00797e67a016c88b138
Nikita Mamay <nikitamamay@mail.ru>
2026-02-03 16:46:48 +0500

    доработано открытие картинок в модальных окнах; и прочее

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,35 +3,91 @@ let modal_bg = Object.assign(document.getElementById("modal-background"), {
 		modal_bg.style.display = "none";
 	},
 	open: () => {
-		modal_bg.style.display = "flex";
+		modal_bg.style = "";
 	},
+	current_el: null,
 });
-let modal_content = document.getElementById("modal-content");
 modal_bg.onclick = (event) => {
 	if (event.target === modal_bg) {
 		modal_bg.close();
 	}
 }
+let modal_content = document.getElementById("modal-content");
+let modal_image = document.getElementById("modal-image");
 
 
 const open_modal = (content) => {
+	modal_image.src = "";
 	modal_content.innerHTML = content;
 	modal_bg.open();
 }
 
-const open_modal_img = (url, text = "") => {
-	open_modal(`<img src="${url}">${text}`)
+const open_modal_img = (img_src, text = "") => {
+	modal_image.src = img_src;
+	modal_content.innerHTML = text;
+	modal_bg.open();
+}
+
+const modal_switch = (increment = 0) => {
+	let i = 0;
+	let list = document.querySelectorAll(".scalable-with-modal");
+	for (; i < list.length; ++i) {
+		let el = list[i];
+		if (el == modal_bg.current_el) {
+			break;
+		}
+	}
+
+	i += increment;
+	if (i < 0) { i = list.length - 1 }
+	if (i >= list.length) { i = 0 }
+
+	console.log(i, list[i])
+
+	list[i].show_in_modal();
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
 	for (let el of document.querySelectorAll(".scalable-with-modal")) {
-		let img = el.querySelector(".gallery-image")
-		img.style.opacity = 0;
-		let div = img.parentElement;
-		div.style.backgroundImage = `url(${img.src})`;
-		text = `<p>${img.alt}</p>`
-		// div.onclick = () => open_modal_img(img.src, text);
-		div.onclick = () => open_modal(el.innerHTML);
+		let img = null;
+		if (el.tagName == "IMG") {
+			img = el;
+		} else {
+			console.error(`Not an image:`, el);
+			continue;
+			img = el.querySelector("img");
+			if (img === null) {
+				console.error(`Cannot find image in`, el);
+				continue;
+			}
+		}
+
+		img.show_in_modal = () => {
+			let content = "";
+
+			try {
+				let parent = img.parentElement;
+				if (parent.classList.contains("img-holder")) {
+					parent = parent.parentElement;
+				}
+
+				if (parent !== null) {
+					parent.querySelectorAll(".scalable-additional-content").forEach((el) => { content += el.outerHTML; });
+				}
+			} catch (error) {
+				console.error(error);
+			}
+
+			// let additional_content_id = el.getAttribute("additional-content-id");
+
+			// if (additional_content_id != null) {
+			// 	content = document.getElementById(additional_content_id);
+			// }
+
+			modal_bg.current_el = el;
+			open_modal_img(img.src, content);
+		};
+		img.addEventListener("click", img.show_in_modal);
 	}
 })
